a7242911463b224fea0d4cf9db7dd57a7c29e8c0
Alberto <alberto@alberto-1011PX.(none)>
2014-06-14 15:45:58 +0200

    relazione

--- tesi_14gg/file_filtrati/Script1.sh
+++
@@ -1,4 +0,0 @@
-#controllo se la riga inizia con Internet Protocol Version 4
-echo $line | cut -c 1-150 | grep "Internet Protocol Version 4" >> $repo
-#controllo se la riga inizia con Transmission Control Protocol
-echo $line | cut -c 1-150 | grep "Transmission Control Protocol" >> $repo
